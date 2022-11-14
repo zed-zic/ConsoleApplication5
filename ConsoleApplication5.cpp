9071505d7f2825a010b2548fe141c3fe4a3ef08b
zed-zic <sanchezzz1597@gmail.com>
2022-11-15 01:28:34 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+std::pair <int, int> inputNumbers();
+int max(std::pair <int, int> numbers);
+void printAnswer(int maxNumber);
+
+int main() {
+    printAnswer(max(inputNumbers()));
+    return 0;
+}
+
+std::pair <int, int> inputNumbers() {
+    std::pair <int, int> numbers;
+    std::cin >> numbers.first >> numbers.second;
+    return numbers;
+}
+
+int max(std::pair <int, int> numbers) {
+    return numbers.first > numbers.second ? numbers.first : numbers.second;
+}
+
+void printAnswer(int maxNumber) {
+    std::cout << maxNumber;
+}
